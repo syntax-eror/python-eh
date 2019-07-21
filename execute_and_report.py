db76b34f7dd4cdd389d8a0b65bceb7f15a6580c9
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-07-21 16:37:15 -0700

    Update execute_and_report.py

--- execute_and_report.py
+++ execute_and_report.py
@@ -3,6 +3,16 @@
 import subprocess, smtplib
 
 def send_email(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587) #creates instance of an SMTP server using SMTP lib
+    #using gmail's SMPT server on port 587
+    
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    #(from_addr, to_addr, message)
+    
+    server.quit()
+    
 
 command = "msg * Test Message"
 subprocess.Popen(command, shell=True)
