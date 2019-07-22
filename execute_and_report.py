14e7d5ecf8d567859c5f699ff0c24b0a9c6092d6
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-07-22 10:57:20 -0700

    Update execute_and_report.py

--- execute_and_report.py
+++ execute_and_report.py
@@ -5,6 +5,8 @@ import subprocess, smtplib
 def send_email(email, password, message):
     server = smtplib.SMTP("smtp.gmail.com", 587) #creates instance of an SMTP server using SMTP lib
     #using gmail's SMPT server on port 587
+    #need to enable Less secure app access in Google account settings if using Gmail
+    #https://myaccount.google.com/security
     
     server.starttls()
     server.login(email, password)
