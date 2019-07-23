88f87fcbc403fdf2c0e0ce0a20a3a36ba2cb3fb3
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-07-22 22:31:49 -0700

    Update download.py

--- download.py
+++ download.py
@@ -3,6 +3,7 @@ import requests #http requests through python
 
 def download(url):
     get_response = requests.get(url)
-    print(get_response)
+    print(get_response) #returns response object
+    print(get_response.content) #show content of response object
     
 download("https://rarlab.com/rar/wrar571.exe")
