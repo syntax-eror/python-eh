70ba110571778e4f2e63696eec202d60d5fefbd4
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-07-23 22:45:29 -0700

    Update download.py

--- download.py
+++ download.py
@@ -3,7 +3,12 @@ import requests #http requests through python
 
 def download(url):
     get_response = requests.get(url)
+    file_name = url.split("/")[-1] #split with / as delim -1 for last element in list
+    print(file_name)
     print(get_response) #returns response object
     print(get_response.content) #show content of response object
+    with open("newfile", "wb") as out_file: #wb - open file for writing as binary
+        out_file.write("test")
     
-download("https://rarlab.com/rar/wrar571.exe")
+#download("https://rarlab.com/rar/wrar571.exe")
+download("http://vulnweb.com/acunetix-logo.png")
