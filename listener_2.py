1a0b623cbfbba9616d8afe3bd81136fe59c16510
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-08-15 20:47:17 -0700

    Update listener_2.py

--- listener_2.py
+++ listener_2.py
@@ -4,11 +4,12 @@ import socket
 
 listen_ip = input("Enter IP Address to listen on: ")
 listen_port = input("Enter port to listen on: ")
+
 listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 #setsockopt = method for setting socket options
 #(level, attribute, value)
-#this allows reuse of sockets
+#this allows reuse of sockets, so they will persist through lost connection
 listener.bind((listen_ip, listen_port))
 listener.listen(0) #specify backlog - number of connections that can be queued before it resets
 listener.accept() #specify to listen on that port
