82d4906ff56a31e609fe92922a67882100f7c6f4
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-09-13 16:18:54 -0700

    Create crawler_directories.py

---
+++ crawler_directories.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import requests
+
+def request(url):
+    try:
+        return requests.get("http://" + url)
+        print(get_response)
+    except requests.exceptions.ConnectionError:
+        #this is the type of error pythong returns if it encounters an issue querying the url
+        print("Error, check url")
+        pass
+    
+subdomains_list = raw_input("Enter exact path of word list for subdomains: ")
+#location of wordlist file containing subdomains to check
+
+target_url = "google.com"
+try:
+    with open(subdomains_list, "r") as wordlist_file:
+        for line in wordlist_file:
+            word = line.strip() #strip - built in method to work with strings; removes whitespace
+            test_url = word + "." + target_url
+            response = request(test_url)
+            if response:
+                print("[+] Found subdomain: " + test_url)
+except:
+    print("File not found, check path and filename")
