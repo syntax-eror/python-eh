0ba9d047235e720a697b39d41a6212b7f2b19239
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-09-13 15:08:04 -0700

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -10,3 +10,13 @@ def request(url):
         #this is the type of error pythong returns if it encounters an issue querying the url
         print("Error, check url")
         pass
+    
+try:
+    subdomains_list = raw_input("Enter exact path of word list for subdomains: ")
+except:
+    print("File location not found, check path")
+    
+with open(subdomains_list, "r") as wordlist_file:
+    for line in wordlist_file:
+        print(line)
+        
