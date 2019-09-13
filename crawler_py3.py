71c2f51018ae84942754819d36375b8afb12a9e6
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-09-13 15:16:29 -0700

    Create crawler_py3.py

---
+++ crawler_py3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python 3
+
+import requests
+
+def request(url):
+    try:
+        return requests.get("http://" + url)
+        print(get_response)
+    except requests.exceptions.ConnectionError:
+        #this is the type of error pythong returns if it encounters an issue querying the url
+        print("Error, check url")
+        pass
+    
+try:
+    subdomains_list = input("Enter exact path of word list for subdomains: ")
+except:
+    print("File location not found, check path")
+    
+with open(subdomains_list, "r") as wordlist_file:
+    for line in wordlist_file:
+        print(line)
+        
