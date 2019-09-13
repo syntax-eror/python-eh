55a872480b246ac9871f27dcf4615f2c40beb1c4
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-09-13 16:19:49 -0700

    Update crawler_directories.py

--- crawler_directories.py
+++ crawler_directories.py
@@ -19,9 +19,9 @@ try:
     with open(subdomains_list, "r") as wordlist_file:
         for line in wordlist_file:
             word = line.strip() #strip - built in method to work with strings; removes whitespace
-            test_url = word + "." + target_url
+            test_url = target_url + "/" + word #look for directories
             response = request(test_url)
             if response:
-                print("[+] Found subdomain: " + test_url)
+                print("[+] Found directory: " + test_url)
 except:
     print("File not found, check path and filename")
