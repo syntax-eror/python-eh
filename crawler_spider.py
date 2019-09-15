c09003f69bdc5b58daa57768e60a2100f376f49b
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-09-15 14:52:39 -0700

    Update crawler_spider.py

--- crawler_spider.py
+++ crawler_spider.py
@@ -2,18 +2,27 @@
 
 import requests
 
-def request(url):
-    try:
-        return requests.get("http://" + url)
-    except requests.exceptions.ConnectionError:
+#def request(url):
+    #try:
+        #return requests.get("http://" + url)
+    #except requests.exceptions.ConnectionError:
         #this is the type of error python returns if it encounters an issue querying the url
         #print("Error, check url")
-        pass
-
+        #pass
+        
 target_url = raw_input("Enter domain: ")
 
-response = request(target_url)
+#response = request(target_url)
 
-href_links = re.findall('(?:href=")(.*)"') #?: - non-matching
 
-print(response.content) #.content is a method of response that will return the content of the request from requests library
+def extract_links(url):
+    response = requests.get(url)
+    return re.findall('(?:href=")(.*?)"', response.content)
+    #regex to find all part of a href tag, up to the last double quote <a href="https://example.com/subdir/">
+    #split into two groups with () - href part of the expression is placed in different group
+    #?: - non-matching - make href part non-matching
+    #? - make group non-greedy - will match up until first double quote it finds after the opening "
+
+#print(response.content) #.content is a method of response that will return the content of the request from requests library
+href_links = extract_links(target_url)
+print(href_links)
