3b88488c6c2fa867576b892f61ce0103e57c7f82
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-09-20 12:48:53 -0700

    Update crawler_extractforms.py

--- crawler_extractforms.py
+++ crawler_extractforms.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from bs4 import BeautifulSoup #pip install beautifulsoup4 if needed
+#module that allows parsing of HTML - can more easily extract specific elements from pages than using regex
 import requests
 
 def request(url):
@@ -11,4 +12,6 @@ def request(url):
 
 url = raw_input("Enter target URL: ")
 response = request(url)
-print(response.content)
+
+parsed_html = BeautifulSoup(response.content)
+forms_list = parsed_html.findAll("form") #substitute form with any element you want to locate on page
