6fb0550e2602f4b699901951ed66a37517632425
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-09-22 15:39:26 -0700

    Update crawler_extractforms.py

--- crawler_extractforms.py
+++ crawler_extractforms.py
@@ -15,4 +15,10 @@ response = request(url)
 
 parsed_html = BeautifulSoup(response.content, features="lxml")
 forms_list = parsed_html.findAll("form") #substitute form with any element you want to locate on page, ie "blockquote"
-print(forms_list[0])
+
+for form in forms_list:
+    action = form.get("action") #"action" is the name of the tag in the HTML for specifying what page should do
+    #i.e, form id="lookup" action="index.php?page=target-page.php"
+    print(action)
+
+#print(forms_list[0])
