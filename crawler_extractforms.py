a89fc797acf9d9c424781bf37519e7e0af2727bf
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-09-22 15:50:14 -0700

    Update crawler_extractforms.py

--- crawler_extractforms.py
+++ crawler_extractforms.py
@@ -19,6 +19,8 @@ forms_list = parsed_html.findAll("form") #substitute form with any element you w
 for form in forms_list:
     action = form.get("action") #"action" is the name of the tag in the HTML for specifying what page should do
     #i.e, form id="lookup" action="index.php?page=target-page.php"
+    method = form.get("method") #same thing for method tag in code of web page
     print(action)
+    print(method)
 
 #print(forms_list[0])
