7f9bdf483b5d6981f9063e3bb89c7443ab82d717
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-10-05 18:47:37 -0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -16,11 +16,11 @@ class Scanner:
         for link in href_links:
             link = urlparse.urljoin(url, link) #urlparse is library that will take relative links and convert to full links
             #this converts relative links like /onpage/ to full links https://example.com/onpage
-
-        if "#" in link: #if relative link that redirects to another part of same page (ex: ex.com/#about)
-            link = link.split("#")[0] #split link into two elements, 0 - first index - everything before #
-
-        if self.target_url in link and link not in self.target_links: #if not an external link and not already added to list
-            self.target_links.append(link)
-            print(link)
-            self.crawl(link) #recursive function to continue checking each page on site for addiitional links
+            
+            if "#" in link: #if relative link that redirects to another part of same page (ex: ex.com/#about)
+                link = link.split("#")[0] #split link into two elements, 0 - first index - everything before #
+                
+            if self.target_url in link and link not in self.target_links: #if not an external link and not already added to list
+                self.target_links.append(link)
+                print(link)
+                self.crawl(link) #recursive function to continue checking each page on site for addiitional links
