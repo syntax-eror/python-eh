c7ee8b7b13f73e091c794aa701356d7ec63902ef
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-10-05 19:35:08 -0700

    Update vuln_scanner.py

--- vuln_scanner.py
+++ vuln_scanner.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
-import scanner
+import requests, scanner
 
-target_url = "http://10.0.2.4/mutillidae/"
+target_url = "http://10.0.2.4/dvwa/"
+data_dict = {"username:" "admin", "password": "password", "Login": "submit"}
+response = requests.post(target_url, data=data_dict)
+
 vuln_scan = scanner.Scanner(target_url)
+response = vuln_scan.session.post(target_url, data=data_dict)
 vuln_scan.crawl() #can pass it with no arguments if you use updated scanner.py
 #see documentation within scanner.py
