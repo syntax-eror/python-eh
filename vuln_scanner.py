2529c7a148b7553a8995662012f41783a6f7a898
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-10-05 19:16:34 -0700

    Update vuln_scanner.py

--- vuln_scanner.py
+++ vuln_scanner.py
@@ -4,4 +4,5 @@ import scanner
 
 target_url = "http://10.0.2.4/mutillidae/"
 vuln_scan = scanner.Scanner(target_url)
-vuln_scan.crawl(target_url)
+vuln_scan.crawl() #can pass it with no arguments if you use updated scanner.py
+#see documentation within scanner.py
