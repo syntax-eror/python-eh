d090fa24dacc329313bb9ed53013c37ea74cf28d
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-10-09 13:23:32 -0700

    Update vuln_scanner.py

--- vuln_scanner.py
+++ vuln_scanner.py
@@ -6,7 +6,9 @@ target_url = "http://10.0.2.4/dvwa/"
 data_dict = {"username": "admin", "password": "password", "Login": "submit"}
 response = requests.post(target_url, data=data_dict)
 
-vuln_scan = scanner.Scanner(target_url)
+vuln_scan = scanner.Scanner(target_url) #establishes session using connection method in;
+#scanner.py - this allows use on pages with login forms as opposed to sending invidiual;
+#get and post requests
 response = vuln_scan.session.post(target_url, data=data_dict)
 vuln_scan.crawl() #can pass it with no arguments if you use updated scanner.py
 #see documentation within scanner.py
