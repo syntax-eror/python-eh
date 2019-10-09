0000bffc59cd9103a263886d933faf3091ab4bbf
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2019-10-09 14:11:04 -0700

    Update vuln_scanner.py

--- vuln_scanner.py
+++ vuln_scanner.py
@@ -10,12 +10,13 @@ vuln_scan = scanner.Scanner(target_url, links_to_ignore) #establishes session us
 #scanner.py - this allows use on pages with login forms as opposed to sending invidiual;
 #get and post requests
 vuln_scan.session.post("http://10.0.2.4/dvwa/login.php", data=data_dict)
-results = vuln_scan.extract_forms("http://10.0.2.4/dvwa/vulnerabilities/xss_r/")
-print(results)
+form = vuln_scan.extract_forms("http://10.0.2.4/dvwa/vulnerabilities/xss_r/")
+print(form)
+response = vuln_scan.submit_form(form[0], "testtest", "http://10.0.2.4/dvwa/vulnerabilities/xss_r/")
+print(response.content)
 
 
 #vuln_scan.crawl() #can pass it with no arguments if you use updated scanner.py
 #see documentation within scanner.py
-
 #response = vuln_scan.session.post(target_url, data=data_dict)
 #response = requests.post(target_url, data=data_dict)
